c43908a7a7a95879e6963cd616b5557900b341ba
thanhlc99 <maivanthanh2017603813@gmail.com>
2020-12-25 18:10:50 +0700

    lam den phan js

---
+++ mvthanh/MISA.AMIS.Web/js/common/common.js
@@ -0,0 +1,29 @@
+﻿/**------------------------------------------
+ * format dữ liệu ngày tháng sang ngày/tháng/năm
+ * Created by mvthanh(25/12/2020)
+ * @param {any} date tham số có kiểu dữ liệu bất kỳ
+ */
+function formatDate(date) {
+    var date = new Date(date);//chuyển dữ liệu dạng json string sang datetime
+    if (Number.isNaN(date.getTime())) {
+        return "";
+    }
+    else {
+        var day = date.getDate(),
+            month = date.getMonth() + 1,
+            year = date.getFullYear();
+        day = day < 10 ? '0' + day : day;
+        month = month < 10 ? '0' + month : month;
+        return day + '/' + month + '/' + year;
+    }
+}
+/**-----------------------------------------------
+ * định dạng hiển thị tiền tệ
+ * created by mvthanh(25/12/2020)
+ * @param {any} value money số tiền
+ */
+function formatMoney(value) {
+    value = Number(value)
+    var num = value.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+    return num;
+}
---
+++ mvthanh/MISA.AMIS.Web/js/page/employee.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+function loadData() {
+    //lấy dữ liệu về
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET"
+    }).done(function (res) {
+        var data = res;
+        $.each(data, function (index, item) {
+            var dateOfBirth = item['DateOfBirth'];
+            dateOfBirth = formatDate(dateOfBirth);
+            var salary = item['IdentityNumber'];
+            salary = formatMoney(salary);
+            var tr = $(`<tr>
+                            <td>`+ item['EmployeeCode'] + `</td>
+                            <td>`+ item['FullName'] + `</td>
+                            <td>`+ item['GenderName'] + `</td>
+                            <td >`+ dateOfBirth + `</td>
+                            <td >`+ item['PhoneNumber'] + `</td>
+                            <td >`+ item['Email'] + `</td>
+                            <td>`+ item['PositionName'] + `</td>
+                            <td style="max-width:215px;"><span style="width:215px" title="`+ item['QualificationName'] + `">` + item['QualificationName'] + `</span></td>
+                            <td >`+ salary + `</td>
+                            <td style="max-width:215px;"><span style="width:215px" title="`+ item['Address'] + `">` + item['Address'] + `</span></td>
+                            <td>`+ item['WorkStatusName'] + `</td>
+                        </tr>
+                       `);
+            $('table tbody').append(tr);
+        })
+    }).fail(function (res) {
+
+    })
+    //binding dữ liệu lên table
+}
